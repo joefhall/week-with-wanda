c890fdd660ddfa3fcd67427b27b9f89383c24524
Joe Hall <joefhall@gmail.com>
2019-02-04 13:31:41 +0000

    Update node and npm, recompile

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.react('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
