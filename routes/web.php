6c76baf1c3c48e7d1a7149a538d7573dbba40133
Joe Hall <joefhall@gmail.com>
2019-02-21 16:04:08 +0000

    Remove insecure web routes

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/blah', 'UserController@show');
-Route::middleware('auth')->get('/user', 'UserController@show');
-
 Route::match(['get', 'post'], '/botman', 'BotManController@handle');
 Route::get('/botman/tinker', 'BotManController@tinker');
 
