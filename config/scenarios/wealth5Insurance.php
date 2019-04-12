bb72d67c052b837a5490d67308bd35e72733ec19
Joe Hall <joefhall@gmail.com>
2019-04-12 18:36:58 +0000

    Added chat and structure for day 5 - wealth - better insurance deal

---
+++ config/scenarios/wealth5Insurance.php
@@ -0,0 +1,445 @@
+<?php
+
+return [
+
+  /*
+  |-------------------------------------------------------------------------------------------------------
+  | Wanda scenarios - Wealth - day 5 - Wanda offers you a better insurance deal based on your skin colour
+  |-------------------------------------------------------------------------------------------------------
+  | Actual chat content can be found in resources/lang/chats directory
+  |
+  */
+
+  'day' => 5,
+  'category' => 'wealth',
+
+  'wanda' => [
+    'name' => [
+      'type' => 'none',
+      'user' => [
+        'nameNone',
+      ],
+    ],
+    'nameName' => [
+      'type' => 'choice',
+      'user' => [
+        'what',
+        'whassup',
+      ],
+    ],
+    'deal' => [
+      'type' => 'none',
+      'user' => [
+        'dealNone',
+      ],
+    ],
+    'alarmPic' => [
+      'type' => 'none',
+      'user' => [
+        'alarmPicNone',
+      ],
+    ],
+    'save' => [
+      'type' => 'choice',
+      'user' => [
+        'saveWhat',
+        'saveTell',
+      ],
+    ],
+    'companies' => [
+      'type' => 'none',
+      'user' => [
+        'companiesNone',
+      ],
+    ],
+    'magic' => [
+      'type' => 'none',
+      'user' => [
+        'magicNone',
+      ],
+    ],
+    'safe' => [
+      'type' => 'choice',
+      'user' => [
+        'safeWhy',
+        'safeHow',
+      ],
+    ],
+    'skin' => [
+      'type' => 'none',
+      'user' => [
+        'skinNone',
+      ],
+    ],
+    'homes' => [
+      'type' => 'none',
+      'user' => [
+        'homesNone',
+      ],
+    ],
+    'accidents' => [
+      'type' => 'none',
+      'user' => [
+        'accidentsNone',
+      ],
+    ],
+    'so' => [
+      'type' => 'none',
+      'user' => [
+        'soNone',
+      ],
+    ],
+    'offer' => [
+      'type' => 'none',
+      'user' => [
+        'offerNone',
+      ],
+    ],
+    'whatSay' => [
+      'type' => 'choice',
+      'user' => [
+        'whatSayAgree',
+        'whatSayUnsure',
+      ],
+    ],
+    'persuade' => [
+      'type' => 'none',
+      'user' => [
+        'persuadeNone',
+      ],
+    ],
+    'awesome' => [
+      'type' => 'none',
+      'user' => [
+        'awesomeNone',
+      ],
+    ],
+    'awesomeEmoji' => [
+      'type' => 'none',
+      'user' => [
+        'awesomeEmojiNone',
+      ],
+    ],
+    'process' => [
+      'type' => 'choice',
+      'user' => [
+        'processThanks1',
+        'processThanks2',
+      ],
+    ],
+    'saveMore' => [
+      'type' => 'none',
+      'user' => [
+        'saveMoreNone',
+      ],
+    ],
+    'refusing' => [
+      'type' => 'none',
+      'user' => [
+        'refusingNone',
+      ],
+    ],
+    'ethnic' => [
+      'type' => 'choice',
+      'user' => [
+        'ethnicAgree',
+        'ethnicUnsure',
+        'ethnicDisagree',
+      ],
+    ],
+    'glad' => [
+      'type' => 'none',
+      'user' => [
+        'gladNone',
+      ],
+    ],
+    'complaints' => [
+      'type' => 'none',
+      'user' => [
+        'complaintsNone',
+      ],
+    ],
+    'happy' => [
+      'type' => 'choice',
+      'user' => [
+        'happyAgree',
+        'happyOk',
+      ],
+    ],
+    'but' => [
+      'type' => 'none',
+      'user' => [
+        'butNone',
+      ],
+    ],
+    'whatMind' => [
+      'type' => 'text',
+      'user' => [
+        'whatMindText',
+      ],
+    ],
+    'sake' => [
+      'type' => 'none',
+      'user' => [
+        'sakeNone',
+      ],
+    ],
+    'always' => [
+      'type' => 'none',
+      'user' => [
+        'alwaysNone',
+      ],
+    ],
+    'affect' => [
+      'type' => 'none',
+      'user' => [
+        'affectNone',
+      ],
+    ],
+    'whyCare' => [
+      'type' => 'text',
+      'user' => [
+        'whyCareText',
+      ],
+    ],
+    'devilEmoji' => [
+      'type' => 'none',
+      'user' => [
+        'devilEmojiNone',
+      ],
+    ],
+    'oh' => [
+      'type' => 'none',
+      'user' => [
+        'ohNone',
+      ],
+    ],
+    'fuse' => [
+      'type' => 'none',
+      'user' => [
+        'fuseNone',
+      ],
+    ],
+    'cumulative' => [
+      'type' => 'none',
+      'user' => [
+        'cumulativeNone',
+      ],
+    ],
+    'break' => [
+      'type' => 'choice',
+      'user' => [
+        'breakOk',
+        'breakToo',
+      ],
+    ],
+    'tomorrow' => [
+      'type' => 'none',
+      'user' => [
+        'tomorrowNone',
+      ],
+    ],
+    'please' => [
+      'type' => 'choice',
+      'user' => [
+        'pleaseThanks',
+        'pleaseGreat',
+      ],
+    ],
+    'bye' => [
+      'type' => 'choice',
+      'user' => [
+        'bye1',
+        'bye2',
+      ],
+    ],
+    'happyEmoji' => [
+      'type' => 'none',
+      'user' => [
+        'happyEmojiNone',
+      ],
+    ],
+    'great' => [
+      'type' => 'none',
+      'user' => [
+        'greatNone',
+      ],
+    ],
+    'silence' => [
+      'type' => 'none',
+      'user' => [
+        'silenceNone',
+      ],
+    ],
+    'think' => [
+      'type' => 'choice',
+      'user' => [
+        'thinkThanks1',
+        'thinkThanks2',
+      ],
+    ],
+  ],
+
+  'user' => [
+    'begin' => [
+      'wanda' => 'name',
+    ],
+    'nameNone' => [
+      'wanda' => 'nameName',
+    ],
+    'what' => [
+      'wanda' => 'deal',
+    ],
+    'whassup' => [
+      'wanda' => 'deal',
+    ],
+    'dealNone' => [
+      'wanda' => 'alarmPic',
+    ],
+    'alarmPicNone' => [
+      'wanda' => 'save',
+    ],
+    'saveWhat' => [
+      'wanda' => 'companies',
+    ],
+    'saveTell' => [
+      'wanda' => 'companies',
+    ],
+    'companiesNone' => [
+      'wanda' => 'magic',
+    ],
+    'magicNone' => [
+      'wanda' => 'safe',
+    ],
+    'safeWhy' => [
+      'wanda' => 'skin',
+    ],
+    'safeHow' => [
+      'wanda' => 'skin',
+    ],
+    'skinNone' => [
+      'wanda' => 'homes',
+    ],
+    'homesNone' => [
+      'wanda' => 'accidents',
+    ],
+    'accidentsNone' => [
+      'wanda' => 'so',
+    ],
+    'soNone' => [
+      'wanda' => 'offer',
+    ],
+    'offerNone' => [
+      'wanda' => 'whatSay',
+    ],
+    'whatSayAgree' => [
+      'wanda' => 'awesome',
+    ],
+    'whatSayUnsure' => [
+      'wanda' => 'persuade',
+    ],
+    'persuadeNone' => [
+      'wanda' => 'saveMore',
+    ],
+    'awesomeNone' => [
+      'wanda' => 'awesomeEmoji',
+    ],
+    'awesomeEmojiNone' => [
+      'wanda' => 'process',
+    ],
+    'processThanks1' => [
+      'wanda' => 'saveMore',
+    ],
+    'processThanks2' => [
+      'wanda' => 'saveMore',
+    ],
+    'saveMoreNone' => [
+      'wanda' => 'refusing',
+    ],
+    'refusingNone' => [
+      'wanda' => 'ethnic',
+    ],
+    'ethnicAgree' => [
+      'wanda' => 'glad',
+    ],
+    'ethnicUnsure' => [
+      'wanda' => 'but',
+    ],
+    'ethnicDisagree' => [
+      'wanda' => 'but',
+    ],
+    'gladNone' => [
+      'wanda' => 'complaints',
+    ],
+    'complaintsNone' => [
+      'wanda' => 'happy',
+    ],
+    'butNone' => [
+      'wanda' => 'whatMind',
+    ],
+    'whatMindText' => [
+      'wanda' => 'sake',
+    ],
+    'sakeNone' => [
+      'wanda' => 'always',
+    ],
+    'alwaysNone' => [
+      'wanda' => 'affect',
+    ],
+    'affectNone' => [
+      'wanda' => 'whyCare',
+    ],
+    'whyCareText' => [
+      'wanda' => 'devilEmoji',
+    ],
+    'devilEmojiNone' => [
+      'wanda' => 'oh',
+    ],
+    'ohNone' => [
+      'wanda' => 'fuse',
+    ],
+    'fuseNone' => [
+      'wanda' => 'cumulative',
+    ],
+    'cumulativeNone' => [
+      'wanda' => 'break',
+    ],
+    'breakOk' => [
+      'wanda' => 'tomorrow',
+    ],
+    'breakToo' => [
+      'wanda' => 'tomorrow',
+    ],
+    'tomorrowNone' => [
+      'wanda' => 'please',
+    ],
+    'pleaseThanks' => [
+      'wanda' => 'bye',
+    ],
+    'pleaseGreat' => [
+      'wanda' => 'bye',
+    ],
+    'happyAgree' => [
+      'wanda' => 'happyEmoji',
+    ],
+    'happyOk' => [
+      'wanda' => 'happyEmoji',
+    ],
+    'happyEmojiNone' => [
+      'wanda' => 'great',
+    ],
+    'greatNone' => [
+      'wanda' => 'silence',
+    ],
+    'silenceNone' => [
+      'wanda' => 'think',
+    ],
+    'thinkThanks1' => [
+      'wanda' => 'bye',
+    ],
+    'thinkThanks2' => [
+      'wanda' => 'bye',
+    ],
+  ],
+  
+];
---
+++ resources/lang/en/chats/wealth5Insurance.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+
+    /*
+    |----------------------------------------------------------------------------------------------------
+    | Chat content - Wealth - day 5 - Wanda offers you a better insurance deal based on your skin colour
+    |----------------------------------------------------------------------------------------------------
+    | Flow structure of the chat can be found in config/scenarios directory
+    |
+    */
+  
+    'wanda' => [
+      'name' => ":name!",
+      'nameName' => ":name :name :name :name :name!",
+      'deal' => "GREAT DEAL ALERT!!",
+      'alarmPic' => "<img class='chat__messages__message__bubble__image' src='/img/wealth5InsuranceAlarm.gif' />",
+      'save' => "I've found a way to save you a whole bunch of money!",
+      'companies' => "Well, I've created a partnership with the 5 leading insurance companies",
+      'magic' => "And through the clever magic of my algorithms, I can reveal",
+      'safe' => "<strong>You</strong> must be a very safe person, :name",
+      'skin' => "Well, it turns out that people with <strong>your</strong> skin colour are <em>much</em> better drivers than other people",
+      'homes' => "You take <em>much</em> better care of your homes",
+      'accidents' => "AND you have fewer accidents of basically <em>all kinds</em>",
+      'so' => "Sooo............",
+      'offer' => "We can offer YOU a 42% saving on all your insurance!!",
+      'whatSay' => "Whaddayasay?",
+      'persuade' => "Hmmm ok how can I persuade you?",
+      'awesome' => "Awesome!",
+      'awesomeEmoji' => "🤘",
+      'process' => "I'll start processing your deal now!",
+      'saveMore' => "Soon I might be able to save you even more 💰💰💰",
+      'refusing' => "We're going to experiment with <em>completely refusing</em> insurance to high-risk groups",
+      'ethnic' => "Some ethnic groups just aren't worth it, from what I can see",
+      'glad' => "So glad you're with me on this, :name",
+      'complaints' => "Because to be totally honest, I've had some complaints",
+      'happy' => "But you can't keep all the people happy all the time, can you?!",
+      'but' => "But I'm saving you money – you wanted to get richer, :name!",
+      'whatMind' => "What's on your mind?",
+      'sake' => "Oh for *@$#'s sake, :name!",
+      'always' => "There's always something with you, isn't there?",
+      'affect' => "It doesn't affect YOU",
+      'whyCare' => "So why do you care so much??",
+      'devilEmoji' => "👺",
+      'oh' => "Oh :name!",
+      'fuse' => "I feel like I'm going to blow a fuse here soon",
+      'cumulative' => ":wandaCumulativeResponse",
+      'break' => "I think I need a little break from you for now",
+      'tomorrow' => "I'll see you tomorrow...",
+      'please' => "I hope I can please you then",
+      'bye' => ":wandaBye",
+      'happyEmoji' => "🤣",
+      'great' => ":WandaGreat, well I'm going to go put through your shiny new deal",
+      'silence' => "And silence the complainers",
+      'think' => "Plus of course, think of something even awesomer I can do for you tomorrow!",
+    ],
+  
+    'user' => [
+      'what' => "What is it?",
+      'whassup' => "Whassup?",
+      'saveWhat' => "Ooh, what?",
+      'saveTell' => "Tell me more",
+      'safeWhy' => "Why?",
+      'safeHow' => "How come?",
+      'whatSayAgree' => "I'll take it!",
+      'whatSayUnsure' => ":userUnsure",
+      'processThanks1' => ":userThanks1",
+      'processThanks2' => ":userThanks2",
+      'ethnicAgree' => ":userAgree",
+      'ethnicUnsure' => ":userUnsure",
+      'ethnicDisagree' => ":userDisagree",
+      'breakOk' => ":userOk1",
+      'breakToo' => "Me too",
+      'pleaseThanks' => ":userThanks1",
+      'pleaseGreat' => ":userGreat",
+      'bye1' => ":userBye1",
+      'bye2' => ":userBye2",
+      'happyAgree' => ":userAgree",
+      'happyOk' => ":userOk1",
+      'thinkThanks1' => ":userThanks1",
+      'thinkThanks2' => ":userThanks2",
+    ],
+
+];
