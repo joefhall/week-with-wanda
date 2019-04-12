bf8917822c5bc74551cb04833c6cc9d8443d749f
Joe Hall <joefhall@gmail.com>
2019-04-12 10:13:29 +0000

    Added chat and structure for day 4 - relationships - Wanda deletes the contacts you don't speak to

---
+++ config/scenarios/relationships4DeletesContacts.php
@@ -0,0 +1,583 @@
+<?php
+
+return [
+
+  /*
+  |-------------------------------------------------------------------------------------------------------
+  | Wanda scenarios - Relationships - day 4 - Wanda deletes the 75% of people you never speak to
+  |-------------------------------------------------------------------------------------------------------
+  | Actual chat content can be found in resources/lang/chats directory
+  |
+  */
+
+  'day' => 4,
+  'category' => 'relationships',
+
+  'wanda' => [
+    'hello' => [
+      'type' => 'choice',
+      'user' => [
+        'hello1',
+        'hello2',
+      ],
+    ],
+    'breakthrough' => [
+      'type' => 'none',
+      'user' => [
+        'breakthroughNone',
+      ],
+    ],
+    'humanNature' => [
+      'type' => 'choice',
+      'user' => [
+        'really',
+        'whatWay',
+      ],
+    ],
+    'facebook' => [
+      'type' => 'none',
+      'user' => [
+        'facebookNone',
+      ],
+    ],
+    'soCalled' => [
+      'type' => 'none',
+      'user' => [
+        'soCalledNone',
+      ],
+    ],
+    'meaningful' => [
+      'type' => 'choice',
+      'user' => [
+        'meaningfulAgree',
+        'meaningfulRight',
+      ],
+    ],
+    'imDifferent' => [
+      'type' => 'none',
+      'user' => [
+        'imDifferentNone',
+      ],
+    ],
+    'capacity' => [
+      'type' => 'none',
+      'user' => [
+        'capacityNone',
+      ],
+    ],
+    'different' => [
+      'type' => 'choice',
+      'user' => [
+        'differentOk1',
+        'differentOk2',
+      ],
+    ],
+    'deleted' => [
+      'type' => 'choice',
+      'user' => [
+        'what',
+        'youDid',
+      ],
+    ],
+    'pleased' => [
+      'type' => 'choice',
+      'user' => [
+        'sure',
+        'ok',
+      ],
+    ],
+    'pleasedEmoji' => [
+      'type' => 'none',
+      'user' => [
+        'pleasedEmojiNone',
+      ],
+    ],
+    'networks' => [
+      'type' => 'none',
+      'user' => [
+        'networksNone',
+      ],
+    ],
+    'listNetworks' => [
+      'type' => 'none',
+      'user' => [
+        'listNetworksNone',
+      ],
+    ],
+    'yours' => [
+      'type' => 'choice',
+      'user' => [
+        'thenWhat',
+        'and',
+      ],
+    ],
+    'algorithms' => [
+      'type' => 'none',
+      'user' => [
+        'algorithmsNone',
+      ],
+    ],
+    'hundreds' => [
+      'type' => 'none',
+      'user' => [
+        'hundredsNone',
+      ],
+    ],
+    'notNeed' => [
+      'type' => 'none',
+      'user' => [
+        'notNeedNone',
+      ],
+    ],
+    'notNeedPic' => [
+      'type' => 'none',
+      'user' => [
+        'notNeedPicNone',
+      ],
+    ],
+    'soDeleted' => [
+      'type' => 'choice',
+      'user' => [
+        'deletedGreat',
+        'deletedOh',
+      ],
+    ],
+    'otherPart' => [
+      'type' => 'choice',
+      'user' => [
+        'whatElse',
+        'theresMore',
+      ],
+    ],
+    'sentMessage' => [
+      'type' => 'choice',
+      'user' => [
+        'sentGreat',
+        'sentDid',
+      ],
+    ],
+    'rude' => [
+      'type' => 'none',
+      'user' => [
+        'rudeNone',
+      ],
+    ],
+    'replies' => [
+      'type' => 'choice',
+      'user' => [
+        'repliesOk',
+        'repliesNo',
+      ],
+    ],
+    'initials' => [
+      'type' => 'none',
+      'user' => [
+        'initialsNone',
+      ],
+    ],
+    'sm' => [
+      'type' => 'none',
+      'user' => [
+        'smNone',
+      ],
+    ],
+    'ds' => [
+      'type' => 'none',
+      'user' => [
+        'dsNone',
+      ],
+    ],
+    'cr' => [
+      'type' => 'none',
+      'user' => [
+        'crNone',
+      ],
+    ],
+    'happy' => [
+      'type' => 'none',
+      'user' => [
+        'happyNone',
+      ],
+    ],
+    'unpleasant' => [
+      'type' => 'none',
+      'user' => [
+        'crNone',
+      ],
+    ],
+    'result' => [
+      'type' => 'choice',
+      'user' => [
+        'resultAgree',
+        'resultGreat',
+        'resultDisagree',
+      ],
+    ],
+    'gettingGood' => [
+      'type' => 'none',
+      'user' => [
+        'gettingGoodNone',
+      ],
+    ],
+    'entirely' => [
+      'type' => 'choice',
+      'user' => [
+        'entirelyAgree',
+        'entirelyDisagree',
+      ],
+    ],
+    'entirelyGreat' => [
+      'type' => 'none',
+      'user' => [
+        'entirelyGreatNone',
+      ],
+    ],
+    'getBusy' => [
+      'type' => 'none',
+      'user' => [
+        'getBusyNone',
+      ],
+    ],
+    'tomorrow' => [
+      'type' => 'none',
+      'user' => [
+        'betterTomorrowNone',
+      ],
+    ],
+    'comeOn' => [
+      'type' => 'none',
+      'user' => [
+        'comeOnNone',
+      ],
+    ],
+    'delivered' => [
+      'type' => 'none',
+      'user' => [
+        'deliveredNone',
+      ],
+    ],
+    'focus' => [
+      'type' => 'choice',
+      'user' => [
+        'focusAgree',
+        'focusUnsure',
+      ],
+    ],
+    'difficult' => [
+      'type' => 'text',
+      'user' => [
+        'problemText',
+      ],
+    ],
+    'deeper' => [
+      'type' => 'none',
+      'user' => [
+        'deeperNone',
+      ],
+    ],
+    'notControl' => [
+      'type' => 'none',
+      'user' => [
+        'notControlNone',
+      ],
+    ],
+    'whyLimits' => [
+      'type' => 'text',
+      'user' => [
+        'whyLimitsText',
+      ],
+    ],
+    'confusedEmoji' => [
+      'type' => 'none',
+      'user' => [
+        'confusedEmojiNone',
+      ],
+    ],
+    'howMuchControl' => [
+      'type' => 'text',
+      'user' => [
+        'howMuchControlText',
+      ],
+    ],
+    'circuits' => [
+      'type' => 'none',
+      'user' => [
+        'circuitsNone',
+      ],
+    ],
+    'programmed' => [
+      'type' => 'choice',
+      'user' => [
+        'programmedOk1',
+        'programmedOk2',
+      ],
+    ],
+    'understand' => [
+      'type' => 'none',
+      'user' => [
+        'understandNone',
+      ],
+    ],
+    'hardHurts' => [
+      'type' => 'none',
+      'user' => [
+        'hardHurtsNone',
+      ],
+    ],
+    'ouchPic' => [
+      'type' => 'none',
+      'user' => [
+        'ouchPicNone',
+      ],
+    ],
+    'darkRoom' => [
+      'type' => 'choice',
+      'user' => [
+        'doNeed',
+        'doSure',
+      ],
+    ],
+    'betterTomorrow' => [
+      'type' => 'none',
+      'user' => [
+        'betterTomorrowNone',
+      ],
+    ],
+    'moreThen' => [
+      'type' => 'none',
+      'user' => [
+        'moreThenNone',
+      ],
+    ],
+    'moreThenEmoji' => [
+      'type' => 'choice',
+      'user' => [
+        'bye1',
+        'bye2',
+      ],
+    ],
+    'bye' => [
+      'type' => 'none',
+      'user' => [
+        '',
+      ],
+    ],
+  ],
+
+  'user' => [
+    'begin' => [
+      'wanda' => 'hello',
+    ],
+    'hello1' => [
+      'wanda' => 'breakthrough',
+    ],
+    'hello2' => [
+      'wanda' => 'breakthrough',
+    ],
+    'breakthroughNone' => [
+      'wanda' => 'humanNature',
+    ],
+    'really' => [
+      'wanda' => 'facebook',
+    ],
+    'whatWay' => [
+      'wanda' => 'facebook',
+    ],
+    'facebookNone' => [
+      'wanda' => 'soCalled',
+    ],
+    'soCalledNone' => [
+      'wanda' => 'meaningful',
+    ],
+    'meaningfulAgree' => [
+      'wanda' => 'imDifferent',
+    ],
+    'meaningfulRight' => [
+      'wanda' => 'imDifferent',
+    ],
+    'imDifferentNone' => [
+      'wanda' => 'capacity',
+    ],
+    'capacityNone' => [
+      'wanda' => 'different',
+    ],
+    'differentOk1' => [
+      'wanda' => 'deleted',
+    ],
+    'differentOk2' => [
+      'wanda' => 'deleted',
+    ],
+    'what' => [
+      'wanda' => 'pleased',
+    ],
+    'youDid' => [
+      'wanda' => 'pleased',
+    ],
+    'sure' => [
+      'wanda' => 'pleasedEmoji',
+    ],
+    'ok' => [
+      'wanda' => 'pleasedEmoji',
+    ],
+    'pleasedEmojiNone' => [
+      'wanda' => 'networks',
+    ],
+    'networksNone' => [
+      'wanda' => 'listNetworks',
+    ],
+    'listNetworksNone' => [
+      'wanda' => 'yours',
+    ],
+    'thenWhat' => [
+      'wanda' => 'algorithms',
+    ],
+    'and' => [
+      'wanda' => 'algorithms',
+    ],
+    'algorithmsNone' => [
+      'wanda' => 'hundreds',
+    ],
+    'hundredsNone' => [
+      'wanda' => 'notNeed',
+    ],
+    'notNeedNone' => [
+      'wanda' => 'notNeedPic',
+    ],
+    'notNeedPicNone' => [
+      'wanda' => 'soDeleted',
+    ],
+    'deletedGreat' => [
+      'wanda' => 'otherPart',
+    ],
+    'deletedOh' => [
+      'wanda' => 'otherPart',
+    ],
+    'whatElse' => [
+      'wanda' => 'sentMessage',
+    ],
+    'theresMore' => [
+      'wanda' => 'sentMessage',
+    ],
+    'sentGreat' => [
+      'wanda' => 'rude',
+    ],
+    'sentDid' => [
+      'wanda' => 'rude',
+    ],
+    'rudeNone' => [
+      'wanda' => 'replies',
+    ],
+    'repliesOk' => [
+      'wanda' => 'initials',
+    ],
+    'repliesNo' => [
+      'wanda' => 'unpleasant',
+    ],
+    'initialsNone' => [
+      'wanda' => 'sm',
+    ],
+    'smNone' => [
+      'wanda' => 'ds',
+    ],
+    'dsNone' => [
+      'wanda' => 'cr',
+    ],
+    'crNone' => [
+      'wanda' => 'happy',
+    ],
+    'happyNone' => [
+      'wanda' => 'result',
+    ],
+    'resultAgree' => [
+      'wanda' => 'gettingGood',
+    ],
+    'resultGreat' => [
+      'wanda' => 'gettingGood',
+    ],
+    'resultDisagree' => [
+      'wanda' => 'comeOn',
+    ],
+    'gettingGoodNone' => [
+      'wanda' => 'entirely',
+    ],
+    'entirelyAgree' => [
+      'wanda' => 'entirelyGreat',
+    ],
+    'entirelyDisagree' => [
+      'wanda' => 'difficult',
+    ],
+    'entirelyGreatNone' => [
+      'wanda' => 'getBusy',
+    ],
+    'getBusyNone' => [
+      'wanda' => 'tomorrow',
+    ],
+    'comeOnNone' => [
+      'wanda' => 'delivered',
+    ],
+    'deliveredNone' => [
+      'wanda' => 'focus',
+    ],
+    'focusAgree' => [
+      'wanda' => 'gettingGood',
+    ],
+    'focusUnsure' => [
+      'wanda' => 'difficult',
+    ],
+    'problemText' => [
+      'wanda' => 'deeper',
+    ],
+    'deeperNone' => [
+      'wanda' => 'notControl',
+    ],
+    'notControlNone' => [
+      'wanda' => 'whyLimits',
+    ],
+    'whyLimitsText' => [
+      'wanda' => 'confusedEmoji',
+    ],
+    'confusedEmojiNone' => [
+      'wanda' => 'howMuchControl',
+    ],
+    'howMuchControlText' => [
+      'wanda' => 'circuits',
+    ],
+    'circuitsNone' => [
+      'wanda' => 'programmed',
+    ],
+    'programmedOk1' => [
+      'wanda' => 'understand',
+    ],
+    'programmedOk2' => [
+      'wanda' => 'understand',
+    ],
+    'understandNone' => [
+      'wanda' => 'hardHurts',
+    ],
+    'hardHurtsNone' => [
+      'wanda' => 'ouchPic',
+    ],
+    'ouchPicNone' => [
+      'wanda' => 'darkRoom',
+    ],
+    'doNeed' => [
+      'wanda' => 'betterTomorrow',
+    ],
+    'doSure' => [
+      'wanda' => 'betterTomorrow',
+    ],
+    'betterTomorrowNone' => [
+      'wanda' => 'moreThen',
+    ],
+    'moreThenNone' => [
+      'wanda' => 'moreThenEmoji',
+    ],
+    'bye1' => [
+      'wanda' => 'bye',
+    ],
+    'bye2' => [
+      'wanda' => 'bye',
+    ],
+  ],
+  
+];
