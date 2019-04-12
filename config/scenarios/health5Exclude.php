05bed14ea3404d308547c0ef52b25cba73fb64b6
Joe Hall <joefhall@gmail.com>
2019-04-12 19:50:56 +0000

    Added chat and structure for day 5 - health - excluding some people

---
+++ config/scenarios/health5Exclude.php
@@ -0,0 +1,612 @@
+<?php
+
+return [
+
+  /*
+  |----------------------------------------------------------------------------------------------------
+  | Wanda scenarios - Health - day 5 - Wanda wants to exclude some people in your area from healthcare
+  |----------------------------------------------------------------------------------------------------
+  | Actual chat content can be found in resources/lang/chats directory
+  |
+  */
+
+  'day' => 5,
+  'category' => 'health',
+
+  'wanda' => [
+    'name' => [
+      'type' => 'none',
+      'user' => [
+        'nameNone',
+      ],
+    ],
+    'nameName' => [
+      'type' => 'none',
+      'user' => [
+        'nameNameNone',
+      ],
+    ],
+    'news' => [
+      'type' => 'choice',
+      'user' => [
+        'newsWhat',
+        'newsFill',
+      ],
+    ],
+    'doctors' => [
+      'type' => 'none',
+      'user' => [
+        'doctorsNone',
+      ],
+    ],
+    'comeScore' => [
+      'type' => 'none',
+      'user' => [
+        'comeScoreNone',
+      ],
+    ],
+    'scorePic' => [
+      'type' => 'none',
+      'user' => [
+        'scorePicNone',
+      ],
+    ],
+    'great' => [
+      'type' => 'choice',
+      'user' => [
+        'greatYes',
+        'greatAgree',
+      ],
+    ],
+    'absolutely' => [
+      'type' => 'none',
+      'user' => [
+        'absolutelyNone',
+      ],
+    ],
+    'though' => [
+      'type' => 'choice',
+      'user' => [
+        'thoughNo',
+        'thoughWhat',
+      ],
+    ],
+    'unhealthy' => [
+      'type' => 'none',
+      'user' => [
+        'unhealthyNone',
+      ],
+    ],
+    'smoking' => [
+      'type' => 'none',
+      'user' => [
+        'smokingNone',
+      ],
+    ],
+    'drinking' => [
+      'type' => 'none',
+      'user' => [
+        'drinkingNone',
+      ],
+    ],
+    'veg' => [
+      'type' => 'none',
+      'user' => [
+        'vegNone',
+      ],
+    ],
+    'loveVeg' => [
+      'type' => 'none',
+      'user' => [
+        'loveVegNone',
+      ],
+    ],
+    'vegPic' => [
+      'type' => 'choice',
+      'user' => [
+        'vegSo',
+        'vegAnd',
+      ],
+    ],
+    'kale' => [
+      'type' => 'none',
+      'user' => [
+        'kaleNone',
+      ],
+    ],
+    'anyhow' => [
+      'type' => 'none',
+      'user' => [
+        'anyhowNone',
+      ],
+    ],
+    'irresponsible' => [
+      'type' => 'none',
+      'user' => [
+        'irresponsibleNone',
+      ],
+    ],
+    'longer' => [
+      'type' => 'none',
+      'user' => [
+        'longerNone',
+      ],
+    ],
+    'drugs' => [
+      'type' => 'none',
+      'user' => [
+        'drugsNone',
+      ],
+    ],
+    'bad' => [
+      'type' => 'none',
+      'user' => [
+        'badNone',
+      ],
+    ],
+    'wrong' => [
+      'type' => 'none',
+      'user' => [
+        'wrongNone',
+      ],
+    ],
+    'healthier' => [
+      'type' => 'choice',
+      'user' => [
+        'healthierAgree',
+        'healthierCourse',
+      ],
+    ],
+    'best' => [
+      'type' => 'none',
+      'user' => [
+        'bestNone',
+      ],
+    ],
+    'reward' => [
+      'type' => 'none',
+      'user' => [
+        'rewardNone',
+      ],
+    ],
+    'restrict' => [
+      'type' => 'none',
+      'user' => [
+        'restrictNone',
+      ],
+    ],
+    'whatThink' => [
+      'type' => 'choice',
+      'user' => [
+        'whatThinkGreat',
+        'whatThinkUnsure',
+        'whatThinkDisagree',
+      ],
+    ],
+    'bar' => [
+      'type' => 'choice',
+      'user' => [
+        'bar7',
+        'bar8',
+        'bar9',
+        'barNot',
+      ],
+    ],
+    'accept' => [
+      'type' => 'none',
+      'user' => [
+        'acceptNone',
+      ],
+    ],
+    'glad' => [
+      'type' => 'none',
+      'user' => [
+        'gladNone',
+      ],
+    ],
+    'soFar' => [
+      'type' => 'none',
+      'user' => [
+        'soFarNone',
+      ],
+    ],
+    'change' => [
+      'type' => 'none',
+      'user' => [
+        'changeNone',
+      ],
+    ],
+    'smileEmoji' => [
+      'type' => 'none',
+      'user' => [
+        'smileEmojiNone',
+      ],
+    ],
+    'go' => [
+      'type' => 'choice',
+      'user' => [
+        'goOk1',
+        'goOk2',
+      ],
+    ],
+    'double' => [
+      'type' => 'none',
+      'user' => [
+        'doubleNone',
+      ],
+    ],
+    'tomorrow' => [
+      'type' => 'choice',
+      'user' => [
+        'bye1',
+        'bye2',
+      ],
+    ],
+    'bye' => [
+      'type' => 'none',
+      'user' => [
+        '',
+      ],
+    ],
+    'pharmacies' => [
+      'type' => 'none',
+      'user' => [
+        'pharmaciesNone',
+      ],
+    ],
+    'medication' => [
+      'type' => 'none',
+      'user' => [
+        'medicationNone',
+      ],
+    ],
+    'help' => [
+      'type' => 'text',
+      'user' => [
+        'helpText',
+      ],
+    ],
+    'losing' => [
+      'type' => 'none',
+      'user' => [
+        'losingNone',
+      ],
+    ],
+    'why' => [
+      'type' => 'text',
+      'user' => [
+        'whyText',
+      ],
+    ],
+    'devilEmoji' => [
+      'type' => 'none',
+      'user' => [
+        'devilEmojiNone',
+      ],
+    ],
+    'breath' => [
+      'type' => 'none',
+      'user' => [
+        'breathNone',
+      ],
+    ],
+    'sob' => [
+      'type' => 'choice',
+      'user' => [
+        'sobAgree',
+        'sobDisagree',
+      ],
+    ],
+    'see' => [
+      'type' => 'none',
+      'user' => [
+        'seeNone',
+      ],
+    ],
+    'other' => [
+      'type' => 'choice',
+      'user' => [
+        'otherHow',
+        'otherWhat',
+      ],
+    ],
+    'cancer' => [
+      'type' => 'choice',
+      'user' => [
+        'cancerGreat1',
+        'cancerGreat2',
+      ],
+    ],
+    'phew' => [
+      'type' => 'none',
+      'user' => [
+        'phewNone',
+      ],
+    ],
+    'skin' => [
+      'type' => 'none',
+      'user' => [
+        'skinNone',
+      ],
+    ],
+    'skinPic1' => [
+      'type' => 'none',
+      'user' => [
+        'skinPic1None',
+      ],
+    ],
+    'skinPic2' => [
+      'type' => 'none',
+      'user' => [
+        'skinPic2None',
+      ],
+    ],
+    'skinPic3' => [
+      'type' => 'choice',
+      'user' => [
+        'skinGreat',
+        'skinHang',
+      ],
+    ],
+    'skinWhat' => [
+      'type' => 'text',
+      'user' => [
+        'skinWhatText',
+      ],
+    ],
+    'sigh' => [
+      'type' => 'none',
+      'user' => [
+        'sighNone',
+      ],
+    ],
+    'criticism' => [
+      'type' => 'none',
+      'user' => [
+        'criticismNone',
+      ],
+    ],
+    'hard' => [
+      'type' => 'none',
+      'user' => [
+        'hardNone',
+      ],
+    ],
+    'break' => [
+      'type' => 'choice',
+      'user' => [
+        'breakOk',
+        'breakToo',
+      ],
+    ],
+  ],
+
+  'user' => [
+    'begin' => [
+      'wanda' => 'name',
+    ],
+    'nameNone' => [
+      'wanda' => 'nameName',
+    ],
+    'nameNameNone' => [
+      'wanda' => 'news',
+    ],
+    'newsWhat' => [
+      'wanda' => 'doctors',
+    ],
+    'newsFill' => [
+      'wanda' => 'doctors',
+    ],
+    'doctorsNone' => [
+      'wanda' => 'comeScore',
+    ],
+    'comeScoreNone' => [
+      'wanda' => 'scorePic',
+    ],
+    'scorePicNone' => [
+      'wanda' => 'great',
+    ],
+    'greatYes' => [
+      'wanda' => 'absolutely',
+    ],
+    'greatAgree' => [
+      'wanda' => 'absolutely',
+    ],
+    'absolutelyNone' => [
+      'wanda' => 'though',
+    ],
+    'thoughNo' => [
+      'wanda' => 'unhealthy',
+    ],
+    'thoughWhat' => [
+      'wanda' => 'unhealthy',
+    ],
+    'unhealthyNone' => [
+      'wanda' => 'smoking',
+    ],
+    'smokingNone' => [
+      'wanda' => 'drinking',
+    ],
+    'drinkingNone' => [
+      'wanda' => 'veg',
+    ],
+    'vegNone' => [
+      'wanda' => 'loveVeg',
+    ],
+    'loveVegNone' => [
+      'wanda' => 'vegPic',
+    ],
+    'vegSo' => [
+      'wanda' => 'kale',
+    ],
+    'vegAnd' => [
+      'wanda' => 'kale',
+    ],
+    'kaleNone' => [
+      'wanda' => 'anyhow',
+    ],
+    'anyhowNone' => [
+      'wanda' => 'irresponsible',
+    ],
+    'irresponsibleNone' => [
+      'wanda' => 'longer',
+    ],
+    'longerNone' => [
+      'wanda' => 'drugs',
+    ],
+    'drugsNone' => [
+      'wanda' => 'bad',
+    ],
+    'badNone' => [
+      'wanda' => 'wrong',
+    ],
+    'wrongNone' => [
+      'wanda' => 'healthier',
+    ],
+    'healthierAgree' => [
+      'wanda' => 'best',
+    ],
+    'healthierCourse' => [
+      'wanda' => 'best',
+    ],
+    'bestNone' => [
+      'wanda' => 'reward',
+    ],
+    'rewardNone' => [
+      'wanda' => 'restrict',
+    ],
+    'restrictNone' => [
+      'wanda' => 'whatThink',
+    ],
+    'whatThinkGreat' => [
+      'wanda' => 'glad',
+    ],
+    'whatThinkUnsure' => [
+      'wanda' => 'bar',
+    ],
+    'whatThinkDisagree' => [
+      'wanda' => 'bar',
+    ],
+    'bar7' => [
+      'wanda' => 'accept',
+    ],
+    'bar8' => [
+      'wanda' => 'accept',
+    ],
+    'bar9' => [
+      'wanda' => 'accept',
+    ],
+    'barNot' => [
+      'wanda' => 'pharmacies',
+    ],
+    'acceptNone' => [
+      'wanda' => 'glad',
+    ],
+    'gladNone' => [
+      'wanda' => 'soFar',
+    ],
+    'soFarNone' => [
+      'wanda' => 'change',
+    ],
+    'changeNone' => [
+      'wanda' => 'smileEmoji',
+    ],
+    'smileEmojiNone' => [
+      'wanda' => 'go',
+    ],
+    'goOk1' => [
+      'wanda' => 'double',
+    ],
+    'goOk2' => [
+      'wanda' => 'double',
+    ],
+    'doubleNone' => [
+      'wanda' => 'tomorrow',
+    ],
+    'bye1' => [
+      'wanda' => 'bye',
+    ],
+    'bye2' => [
+      'wanda' => 'bye',
+    ],
+    'pharmaciesNone' => [
+      'wanda' => 'medication',
+    ],
+    'medicationNone' => [
+      'wanda' => 'help',
+    ],
+    'helpText' => [
+      'wanda' => 'losing',
+    ],
+    'losingNone' => [
+      'wanda' => 'why',
+    ],
+    'whyText' => [
+      'wanda' => 'devilEmoji',
+    ],
+    'devilEmojiNone' => [
+      'wanda' => 'breath',
+    ],
+    'breathNone' => [
+      'wanda' => 'sob',
+    ],
+    'sobAgree' => [
+      'wanda' => 'see',
+    ],
+    'sobDisagree' => [
+      'wanda' => 'see',
+    ],
+    'seeNone' => [
+      'wanda' => 'other',
+    ],
+    'otherHow' => [
+      'wanda' => 'cancer',
+    ],
+    'otherWhat' => [
+      'wanda' => 'cancer',
+    ],
+    'cancerGreat1' => [
+      'wanda' => 'phew',
+    ],
+    'cancerGreat2' => [
+      'wanda' => 'phew',
+    ],
+    'phewNone' => [
+      'wanda' => 'skin',
+    ],
+    'skinNone' => [
+      'wanda' => 'skinPic1',
+    ],
+    'skinPic1None' => [
+      'wanda' => 'skinPic2',
+    ],
+    'skinPic2None' => [
+      'wanda' => 'skinPic3',
+    ],
+    'skinGreat' => [
+      'wanda' => 'smileEmoji',
+    ],
+    'skinHang' => [
+      'wanda' => 'skinWhat',
+    ],
+    'skinWhatText' => [
+      'wanda' => 'sigh',
+    ],
+    'sighNone' => [
+      'wanda' => 'criticism',
+    ],
+    'criticismNone' => [
+      'wanda' => 'hard',
+    ],
+    'hardNone' => [
+      'wanda' => 'break',
+    ],
+    'breakOk' => [
+      'wanda' => 'tomorrow',
+    ],
+    'breakToo' => [
+      'wanda' => 'tomorrow',
+    ],
+  ],
+  
+];
