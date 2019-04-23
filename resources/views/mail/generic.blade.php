50333c66faed6cbb51dddbc28a345a5cfe1ed0df
Joe Hall <joefhall@gmail.com>
2019-04-23 11:55:00 +0000

    Added unsubscribe link to email generic template

--- resources/views/mail/generic.blade.php
+++ resources/views/mail/generic.blade.php
@@ -3,3 +3,5 @@
 <p style="color: #D3D3D3;">====================</p>
 
 <p style="color: #D3D3D3;">A Week With Wanda</p>
+
+<p style="color: #D3D3D3;"><a style="color: #D3D3D3;" href="https://weekwithwanda.com/unsubscribe">Unsubscribe</a></p>
