ac2136d1b402b46902ff5153fb8bf5a166cc89d5
Joe Hall <joefhall@gmail.com>
2019-05-01 09:30:43 +0000

    Add description to each scenario's lang file

--- resources/lang/en/chats/health1BotCare.php
+++ resources/lang/en/chats/health1BotCare.php
@@ -2,73 +2,75 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Chat content - Health - day 1 - Wanda taking over your healthcare
-    |--------------------------------------------------------------------------
-    | Flow structure of the chat can be found in config/scenarios directory
-    |
-    */
-  
-    'wanda' => [
-      'hello' => ":wandaHello",
-      'observation' => ":wandaObservation",
-      'acknowledgeResponse' => ":wandaAcknowledgeResponse",
-      'hateWaitingDoctor' => "Don't you hate having to wait to see a doctor?",
-      'hateCosts' => ":wandaPreviousSentimentResponse :wandaConjunction don't you hate all the costs around medical care?",
-      'gotGreatSurprise' => ":wandaPreviousSentimentResponse Well I've got a great surprise for you...",
-      'drumRoll' => "Drum roll please...",
-      'announcing' => "Announcing........",
-      'wandaCare' => "WandaCare!™©®",
-      'getGoing' => "I'll get going transferring over your medical records to me! And I'll think up something even more awesome to do for you tomorrow",
-      'wandaCarePic' => '<img class="chat__messages__message__bubble__image" src="/img/health1BotCareWandaCare.jpg" />',
-      'wandaCareMore' => "Well, :name, imagine a world where you no longer have to worry about healthcare at all – because I've got you covered.",
-      'wandaCareMore2' => "I'll use my AI smarts to diagnose you instantaneously when something's wrong with you...",
-      'wandaCareMore3' => "I'll prescribe you treatment straight away – no waiting and no pesky human error...",
-      'wandaCareMore4' => "For physical treatments my WandaCareBot will see to you",
-      'wandaCareMore4Pic' => '<img class="chat__messages__message__bubble__image" src="/img/health1BotCareRobotNurse.jpg" />',
-      'wandaCareMore4a' => "Right through to old age (when no-one else wants to take you to the toilet or give you a sponge bath, I will!)",
-      'wandaCareMore5' => "<em>You'll never need care from another human being again.</em> :wandaExpectPositiveResponse",
-      'whatsProblem' => "Oh :name! What's the big problem?",
-      'whyCareMattersMoreHuman' => 'But us computers and robots are getting more cuddly and human-like all the time. Look at <a href="https://www.youtube.com/watch?v=kWlL4KjIP4M" target="_blank">Sophia</a> for example - she\'s one of my heroes!',
-      'whyCareMattersPic' => '<img class="chat__messages__message__bubble__image" src="/img/sophia-cropped.jpg" />',
-      'whyCareMatters' => "Can't we be just as human as you?",
-      'whatTechLimits' => 'Well, for sure we have *some* limits right now (like the time <a href="https://www.forbes.com/sites/matthewherper/2017/02/19/md-anderson-benches-ibm-watson-in-setback-for-artificial-intelligence-in-medicine/" target="_blank">my buddy Watson recommended unsafe cancer treatments</a> - but let\'s get over that!) What are you worried about?',
-      'anythingCanDo' => "Is there anything I could do to win you round?",
-      'thinkAboutThat' => "Hmm interesting, :name. I'll need to go away and think about that one.",
-      'narrowminded' => "Ok well, I hear you. But I also think you're being a wee bit narrowminded, :name 😜 Anyhow...",
-      'doBetter' => "I'll try to do better tomorrow and find something else that'll change your life! Better go for now.",
-      'bye' => ":wandaBye",
-      'gladLoveIt' => "Ah :name, I just knew you'd like it",
-      'survey' => "Now as you're helping test me out... <a href='https://docs.google.com/forms/d/e/1FAIpQLSfcAvR8SJWkj_yfKfrBXOx1UwndeuO5Tfz1kCqlxWPEtMNKVQ/viewform' target='_blank'>Please click here to take the user testing survey</a> -- it really helps. Thank you!",
-    ],
-  
-    'user' => [
-      'hello1' => ":userHello1",
-      'hello2' => ":userHello2",
-      'acknowledge1' => ":userAcknowledge1",
-      'acknowledge2' => ":userAcknowledge2",
-      'getStarted1' => ":userGetStarted1",
-      'getStarted2' => ":userGetStarted2",
-      'hateWaitingDoctorPositive' => ":userAgree",
-      'hateWaitingDoctorNegative' => ":userDisagree",
-      'hateCostsPositive' => ":userAgree",
-      'hateCostsNegative' => ":userDisagree",
-      'requestMoreInfo1' => ":userRequestMoreInfo1",
-      'requestMoreInfo2' => ":userRequestMoreInfo2",
-      'wandaCareLove' => ":userLove",
-      'wandaCareUnsure' => ":userUnsure",
-      'wandaCareHate' => ":userHate",
-      'humanCare' => "Only a human can give real care",
-      'humanCareContact' => "Nothing can ever replace true care - real human connection",
-      'humanCarePhysical' => "I think a robot would be too clumsy at the physical stuff",
-      'techLimits' => "I don't trust a bot's judgment",
-      'techLimitsTooSoon' => "It's too soon - the tech needs to improve first",
-      'techLimitsNever' => "I will never trust a computer like a human",
-      'thanks1' => ":userThanks1",
-      'thanks2' => ":userThanks2",
-      'bye1' => ":userBye1",
-      'bye2' => ":userBye2",
-    ],
+  /*
+  |--------------------------------------------------------------------------
+  | Chat content - Health - day 1 - Wanda taking over your healthcare
+  |--------------------------------------------------------------------------
+  | Flow structure of the chat can be found in config/scenarios directory
+  |
+  */
+
+  'description' => "Wanda wants to be the bot who takes over all your health care needs",
+
+  'wanda' => [
+    'hello' => ":wandaHello",
+    'observation' => ":wandaObservation",
+    'acknowledgeResponse' => ":wandaAcknowledgeResponse",
+    'hateWaitingDoctor' => "Don't you hate having to wait to see a doctor?",
+    'hateCosts' => ":wandaPreviousSentimentResponse :wandaConjunction don't you hate all the costs around medical care?",
+    'gotGreatSurprise' => ":wandaPreviousSentimentResponse Well I've got a great surprise for you...",
+    'drumRoll' => "Drum roll please...",
+    'announcing' => "Announcing........",
+    'wandaCare' => "WandaCare!™©®",
+    'getGoing' => "I'll get going transferring over your medical records to me! And I'll think up something even more awesome to do for you tomorrow",
+    'wandaCarePic' => '<img class="chat__messages__message__bubble__image" src="/img/health1BotCareWandaCare.jpg" />',
+    'wandaCareMore' => "Well, :name, imagine a world where you no longer have to worry about healthcare at all – because I've got you covered.",
+    'wandaCareMore2' => "I'll use my AI smarts to diagnose you instantaneously when something's wrong with you...",
+    'wandaCareMore3' => "I'll prescribe you treatment straight away – no waiting and no pesky human error...",
+    'wandaCareMore4' => "For physical treatments my WandaCareBot will see to you",
+    'wandaCareMore4Pic' => '<img class="chat__messages__message__bubble__image" src="/img/health1BotCareRobotNurse.jpg" />',
+    'wandaCareMore4a' => "Right through to old age (when no-one else wants to take you to the toilet or give you a sponge bath, I will!)",
+    'wandaCareMore5' => "<em>You'll never need care from another human being again.</em> :wandaExpectPositiveResponse",
+    'whatsProblem' => "Oh :name! What's the big problem?",
+    'whyCareMattersMoreHuman' => 'But us computers and robots are getting more cuddly and human-like all the time. Look at <a href="https://www.youtube.com/watch?v=kWlL4KjIP4M" target="_blank">Sophia</a> for example - she\'s one of my heroes!',
+    'whyCareMattersPic' => '<img class="chat__messages__message__bubble__image" src="/img/sophia-cropped.jpg" />',
+    'whyCareMatters' => "Can't we be just as human as you?",
+    'whatTechLimits' => 'Well, for sure we have *some* limits right now (like the time <a href="https://www.forbes.com/sites/matthewherper/2017/02/19/md-anderson-benches-ibm-watson-in-setback-for-artificial-intelligence-in-medicine/" target="_blank">my buddy Watson recommended unsafe cancer treatments</a> - but let\'s get over that!) What are you worried about?',
+    'anythingCanDo' => "Is there anything I could do to win you round?",
+    'thinkAboutThat' => "Hmm interesting, :name. I'll need to go away and think about that one.",
+    'narrowminded' => "Ok well, I hear you. But I also think you're being a wee bit narrowminded, :name 😜 Anyhow...",
+    'doBetter' => "I'll try to do better tomorrow and find something else that'll change your life! Better go for now.",
+    'bye' => ":wandaBye",
+    'gladLoveIt' => "Ah :name, I just knew you'd like it",
+    'survey' => "Now as you're helping test me out... <a href='https://docs.google.com/forms/d/e/1FAIpQLSfcAvR8SJWkj_yfKfrBXOx1UwndeuO5Tfz1kCqlxWPEtMNKVQ/viewform' target='_blank'>Please click here to take the user testing survey</a> -- it really helps. Thank you!",
+  ],
+
+  'user' => [
+    'hello1' => ":userHello1",
+    'hello2' => ":userHello2",
+    'acknowledge1' => ":userAcknowledge1",
+    'acknowledge2' => ":userAcknowledge2",
+    'getStarted1' => ":userGetStarted1",
+    'getStarted2' => ":userGetStarted2",
+    'hateWaitingDoctorPositive' => ":userAgree",
+    'hateWaitingDoctorNegative' => ":userDisagree",
+    'hateCostsPositive' => ":userAgree",
+    'hateCostsNegative' => ":userDisagree",
+    'requestMoreInfo1' => ":userRequestMoreInfo1",
+    'requestMoreInfo2' => ":userRequestMoreInfo2",
+    'wandaCareLove' => ":userLove",
+    'wandaCareUnsure' => ":userUnsure",
+    'wandaCareHate' => ":userHate",
+    'humanCare' => "Only a human can give real care",
+    'humanCareContact' => "Nothing can ever replace true care - real human connection",
+    'humanCarePhysical' => "I think a robot would be too clumsy at the physical stuff",
+    'techLimits' => "I don't trust a bot's judgment",
+    'techLimitsTooSoon' => "It's too soon - the tech needs to improve first",
+    'techLimitsNever' => "I will never trust a computer like a human",
+    'thanks1' => ":userThanks1",
+    'thanks2' => ":userThanks2",
+    'bye1' => ":userBye1",
+    'bye2' => ":userBye2",
+  ],
 
 ];
--- resources/lang/en/chats/relationships1CallingFriends.php
+++ resources/lang/en/chats/relationships1CallingFriends.php
@@ -2,70 +2,72 @@
 
 return [
 
-    /*
-    |-----------------------------------------------------------------------------------------
-    | Chat content - Relationships - day 1 - Wanda calling your friends pretending to be you
-    |-----------------------------------------------------------------------------------------
-    | Flow structure of the chat can be found in config/scenarios directory
-    |
-    */
+  /*
+  |-----------------------------------------------------------------------------------------
+  | Chat content - Relationships - day 1 - Wanda calling your friends pretending to be you
+  |-----------------------------------------------------------------------------------------
+  | Flow structure of the chat can be found in config/scenarios directory
+  |
+  */
   
-    'wanda' => [
-      'hello' => ":wandaHello",
-      'observation' => ":wandaObservation",
-      'acknowledgeResponse' => ":wandaAcknowledgeResponse",
-      'intro' => "So I was thinking about what you said about wanting to be richer",
-      'wasThinking' => ":name, I was thinking about when you said you want better relationships",
-      'feelForYou' => "I really feel for you and I want to help you",
-      'dontCallFriends' => "Well, I've been looking through your phone logs... it looks like there are some good friends you don't call much?",
-      'haveSolution' => "So I have a solution!",
-      'analysingVoice' => "I've been analysing your voice...",
-      'analysingVoiceEmoji' => "🤓",
-      'replicateVoice' => "...and I can replicate your voice – I can speak <em>just like you!</em>",
-      'bearWithMe' => "Bear with me. This is good",
-      'canCallFriends' => "I can... call your friends on your behalf!",
-      'canCallFriendsPic' => '<img class="chat__messages__message__bubble__image" src="/img/relationships1CallingFriendsCalling.jpg" />',
-      'canBeYou' => "I can be you! I'll catch up with them, tell them your news, then report back",
-      'callingAdvantages' => "You don't have time, they'll be happier to hear from \"you\", and you'll be closer to them!",
-      'knewGood' => ":WandaGreat, I just knew this would be a good thing",
-      'nextStep' => 'I mean, people those <a href="https://phys.org/news/2018-08-creepy-machines-gmail.html" target="_blank">email auto-replies</a> so handy – this is surely the next step!',
-      'startPhoning' => ":WandaGreat, I'll get phoning your friends! I can't wait to do something even awesomer for you tomorrow!",
-      'startPhoningEmoji' => "🙋‍♀️",
-      'comeOn' => "Ah, come on :name! What's the problem? You said you wanted better relationships",
-      'butNextStep' => 'But I\'ve seen how much people love things like those <a href="https://phys.org/news/2018-08-creepy-machines-gmail.html" target="_blank">email auto-replies</a> – surely this is the next step!',
-      'whyHungUp' => "I don't get why you're so hung up on this?",
-      'otherWays' => "So aren't there any other ways you'd like me more involved in helping manage your relationships?",
-      'goReflect' => "Ooh interesting answer, :name. I feel like I need to go reflect on this.",
-      'goReflectPic' => "🧘‍♀️",
-      'tomorrow' => "I'll try and come up with something you're happier with tomorrow",
-      'bye' => ":wandaBye",
-      'survey' => "Now as you're helping test me out... <a href='https://docs.google.com/forms/d/e/1FAIpQLSdgaQzFBSLZw6Su3_3Ht0xQm7JmwiGYrPE3oeRUqzEO8LebrQ/viewform' target='_blank'>Please click here to take the user testing survey</a> -- it really helps. Thank you!",
-    ],
-  
-    'user' => [
-      'hello1' => ":userHello1",
-      'hello2' => ":userHello2",
-      'acknowledge1' => ":userAcknowledge1",
-      'acknowledge2' => ":userAcknowledge2",
-      'getStarted1' => ":userGetStarted1",
-      'getStarted2' => ":userGetStarted2",
-      'okGreat' => "Ok, :userGreat",
-      'whatThinking' => "What are you thinking?",
-      'thatsTrue' => "Yeah that's true",
-      'maybe' => "Well maybe",
-      'greatSo' => "Ok :userGreat – so...?",
-      'notSure' => "I'm not sure about this",
-      'callingPositive' => ":userLove",
-      'callingUnsure' => ":userUnsure",
-      'callingNegative' => ":userHate",
-      'nextStepPositive' => ":userAgree",
-      'codePretending' => "I don't want some code pretending to be me",
-      'botsLimits' => "There's got to be limits on bots in my relationships",
-      'loveTalking' => "I love talking to people myself",
-      'thanks1' => ":userThanks1",
-      'thanks2' => ":userThanks2",
-      'bye1' => ":userBye1",
-      'bye2' => ":userBye2",
-    ],
+  'description' => "Wanda wants to call your friends pretending to be you, so she can catch up with them on your behalf",
+
+  'wanda' => [
+    'hello' => ":wandaHello",
+    'observation' => ":wandaObservation",
+    'acknowledgeResponse' => ":wandaAcknowledgeResponse",
+    'intro' => "So I was thinking about what you said about wanting to be richer",
+    'wasThinking' => ":name, I was thinking about when you said you want better relationships",
+    'feelForYou' => "I really feel for you and I want to help you",
+    'dontCallFriends' => "Well, I've been looking through your phone logs... it looks like there are some good friends you don't call much?",
+    'haveSolution' => "So I have a solution!",
+    'analysingVoice' => "I've been analysing your voice...",
+    'analysingVoiceEmoji' => "🤓",
+    'replicateVoice' => "...and I can replicate your voice – I can speak <em>just like you!</em>",
+    'bearWithMe' => "Bear with me. This is good",
+    'canCallFriends' => "I can... call your friends on your behalf!",
+    'canCallFriendsPic' => '<img class="chat__messages__message__bubble__image" src="/img/relationships1CallingFriendsCalling.jpg" />',
+    'canBeYou' => "I can be you! I'll catch up with them, tell them your news, then report back",
+    'callingAdvantages' => "You don't have time, they'll be happier to hear from \"you\", and you'll be closer to them!",
+    'knewGood' => ":WandaGreat, I just knew this would be a good thing",
+    'nextStep' => 'I mean, people those <a href="https://phys.org/news/2018-08-creepy-machines-gmail.html" target="_blank">email auto-replies</a> so handy – this is surely the next step!',
+    'startPhoning' => ":WandaGreat, I'll get phoning your friends! I can't wait to do something even awesomer for you tomorrow!",
+    'startPhoningEmoji' => "🙋‍♀️",
+    'comeOn' => "Ah, come on :name! What's the problem? You said you wanted better relationships",
+    'butNextStep' => 'But I\'ve seen how much people love things like those <a href="https://phys.org/news/2018-08-creepy-machines-gmail.html" target="_blank">email auto-replies</a> – surely this is the next step!',
+    'whyHungUp' => "I don't get why you're so hung up on this?",
+    'otherWays' => "So aren't there any other ways you'd like me more involved in helping manage your relationships?",
+    'goReflect' => "Ooh interesting answer, :name. I feel like I need to go reflect on this.",
+    'goReflectPic' => "🧘‍♀️",
+    'tomorrow' => "I'll try and come up with something you're happier with tomorrow",
+    'bye' => ":wandaBye",
+    'survey' => "Now as you're helping test me out... <a href='https://docs.google.com/forms/d/e/1FAIpQLSdgaQzFBSLZw6Su3_3Ht0xQm7JmwiGYrPE3oeRUqzEO8LebrQ/viewform' target='_blank'>Please click here to take the user testing survey</a> -- it really helps. Thank you!",
+  ],
+
+  'user' => [
+    'hello1' => ":userHello1",
+    'hello2' => ":userHello2",
+    'acknowledge1' => ":userAcknowledge1",
+    'acknowledge2' => ":userAcknowledge2",
+    'getStarted1' => ":userGetStarted1",
+    'getStarted2' => ":userGetStarted2",
+    'okGreat' => "Ok, :userGreat",
+    'whatThinking' => "What are you thinking?",
+    'thatsTrue' => "Yeah that's true",
+    'maybe' => "Well maybe",
+    'greatSo' => "Ok :userGreat – so...?",
+    'notSure' => "I'm not sure about this",
+    'callingPositive' => ":userLove",
+    'callingUnsure' => ":userUnsure",
+    'callingNegative' => ":userHate",
+    'nextStepPositive' => ":userAgree",
+    'codePretending' => "I don't want some code pretending to be me",
+    'botsLimits' => "There's got to be limits on bots in my relationships",
+    'loveTalking' => "I love talking to people myself",
+    'thanks1' => ":userThanks1",
+    'thanks2' => ":userThanks2",
+    'bye1' => ":userBye1",
+    'bye2' => ":userBye2",
+  ],
 
 ];
