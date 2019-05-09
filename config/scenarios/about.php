3d6990b448a1a22895a26266c68e9ae34a3f9714
Joe Hall <joefhall@gmail.com>
2019-05-09 13:04:51 +0000

    Create About chat

---
+++ config/scenarios/about.php
@@ -0,0 +1,197 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | About, credits and contact details
+    |--------------------------------------------------------------------------
+    */
+
+    'day' => null,
+    'category' => 'other',
+  
+    'wanda' => [
+      'about1' => [
+        'type' => 'none',
+        'user' => [
+          'about1None',
+        ],
+        'emotion' => 'thumbs-up',
+      ],
+      'about2' => [
+        'type' => 'none',
+        'user' => [
+          'about2None',
+        ],
+      ],
+      'about3' => [
+        'type' => 'none',
+        'user' => [
+          'about3None',
+        ],
+      ],
+      'about4' => [
+        'type' => 'none',
+        'user' => [
+          'about4None',
+        ],
+      ],
+      'about5' => [
+        'type' => 'none',
+        'user' => [
+          'about5None',
+        ],
+      ],
+      'about6' => [
+        'type' => 'none',
+        'user' => [
+          'about6None',
+        ],
+      ],
+      'about7' => [
+        'type' => 'none',
+        'user' => [
+          'about7None',
+        ],
+      ],
+      'about8' => [
+        'type' => 'none',
+        'user' => [
+          'about8None',
+        ],
+        'emotion' => 'heart',
+      ],
+      'about9' => [
+        'type' => 'none',
+        'user' => [
+          'about9None',
+        ],
+      ],
+      'about10' => [
+        'type' => 'none',
+        'user' => [
+          'about10None',
+        ],
+      ],
+      'about11' => [
+        'type' => 'none',
+        'user' => [
+          'about11None',
+        ],
+      ],
+      'about12' => [
+        'type' => 'none',
+        'user' => [
+          'about12None',
+        ],
+      ],
+      'about13' => [
+        'type' => 'none',
+        'user' => [
+          'about13None',
+        ],
+      ],
+      'about14' => [
+        'type' => 'none',
+        'user' => [
+          'about14None',
+        ],
+      ],
+      'about15' => [
+        'type' => 'none',
+        'user' => [
+          'about15None',
+        ],
+      ],
+      'about16' => [
+        'type' => 'none',
+        'user' => [
+          'about16None',
+        ],
+      ],
+      'about17' => [
+        'type' => 'none',
+        'user' => [
+          'about17None',
+        ],
+      ],
+      'about18' => [
+        'type' => 'none',
+        'user' => [
+          'about18None',
+        ],
+      ],
+      'about19' => [
+        'type' => 'end',
+        'user' => [
+          'about19None',
+        ],
+      ],
+    ],
+  
+    'user' => [
+      'begin' => [
+        'wanda' => 'about1',
+      ],
+      'about1None' => [
+        'wanda' => 'about2',
+      ],
+      'about2None' => [
+        'wanda' => 'about3',
+      ],
+      'about3None' => [
+        'wanda' => 'about4',
+      ],
+      'about4None' => [
+        'wanda' => 'about5',
+      ],
+      'about5None' => [
+        'wanda' => 'about6',
+      ],
+      'about6None' => [
+        'wanda' => 'about7',
+      ],
+      'about7None' => [
+        'wanda' => 'about8',
+      ],
+      'about8None' => [
+        'wanda' => 'about9',
+      ],
+      'about9None' => [
+        'wanda' => 'about10',
+      ],
+      'about10None' => [
+        'wanda' => 'about11',
+      ],
+      'about11None' => [
+        'wanda' => 'about12',
+      ],
+      'about12None' => [
+        'wanda' => 'about13',
+      ],
+      'about13None' => [
+        'wanda' => 'about14',
+      ],
+      'about14None' => [
+        'wanda' => 'about15',
+      ],
+      'about15None' => [
+        'wanda' => 'about16',
+      ],
+      'about16None' => [
+        'wanda' => 'about17',
+      ],
+      'about17None' => [
+        'wanda' => 'about18',
+      ],
+      'about18None' => [
+        'wanda' => 'about19',
+      ],
+      'about19None' => [
+        'wanda' => '',
+      ],
+    ],
+
+    
+];
---
+++ resources/lang/en/chats/about.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | About, credits and contact details
+    |--------------------------------------------------------------------------
+    */
+  
+    'wanda' => [
+      'about1' => "Hello! Welcome to A Week With Wanda",
+      'about2' => "It's a web-based game hoping to get more people thinking about the role artificial intelligence (AI) should be playing in our lives",
+      'about3' => "And giving you the chance to share your views",
+      'about4' => "Nothing Wanda 'does' is real – it's all a simulation. But it's inspired by lots of real AI",
+      'about5' => "Some of it is already happening today – in some cases even more extreme things are happening in real life",
+      'about6' => "In other cases Wanda's actions are a combination of existing trends creating something fictional and maybe humorous but (quite possibly) conceivable",
+      'about7' => "It was created by Joe Hall (<a href='mailto:hello@weekwithwanda.com'>hello@weekwithwanda.com</a>) in London, with the support of a <a href='https://blog.mozilla.org/blog/2018/10/24/keeping-ai-accountable-with-science-fiction-documentaries-and-doodles-plus-225000/' target='_blank'>Mozilla Creative Media Award</a>",
+      'about8' => "Joe says: <em>\"An enormous thank you to everyone who has shared research, advised, guided, supported, tried this out and helped make it better. (Any mistakes or misjudgments are my own!)\"</em>",
+      'about9' => "Brett Gaylor, Jenn Beard, Lindsey Frost, Kalpana Simhan, Kevin Zawacki, Anil Kanji and others at the Mozilla Foundation",
+      'about10' => "Paul Johnson and Mary Joyce from Harmony Labs",
+      'about11' => "The other awesome creators in our <a href='https://blog.mozilla.org/blog/2018/10/24/keeping-ai-accountable-with-science-fiction-documentaries-and-doodles-plus-225000/' target='_blank'>Creative Media Awards cohort</a>",
+      'about12' => "Mozilla Fellows Kadija Ferryman and Jason Schultz",
+      'about13' => "Amber Macintyre from Tactical Tech",
+      'about14' => "Research from the <a href='https://ainowinstitute.org' target='_blank'>AI Now Institute</a>, <a href='https://futureoflife.org/' target='_blank'>Internet Health Report</a>, <a href='https://futureoflife.org/' target='_blank'>Future of Life Institute</a> and others",
+      'about15' => "Reporting from the BBC, CNN, Fast Company, Forbes, the Guardian, the Independent, New York Times, TechCrunch, Vice, Wired and others",
+      'about16' => "Kathy Peach who originally suggested the Creative Media Awards",
+      'about17' => "Everyone who tested out the game and gave their feedback",
+      'about18' => "Joe's family, friends and lovely fiancée Johanna for all their support!",
+      'about19' => "(You can close this window to return to the main chat)",
+    ],
+  
+    'user' => [
+      //
+    ],
+
+];
