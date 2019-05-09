3b0962946a5723e7ad36f919af896324f8a19c2f
Joe Hall <joefhall@gmail.com>
2019-05-09 14:43:46 +0000

    Add description to About chat

--- config/scenarios/about.php
+++ config/scenarios/about.php
@@ -2,205 +2,205 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | About, credits and contact details
-    |--------------------------------------------------------------------------
-    */
+  /*
+  |--------------------------------------------------------------------------
+  | About, credits and contact details
+  |--------------------------------------------------------------------------
+  */
 
-    'day' => null,
-    'category' => 'other',
-  
-    'wanda' => [
-      'about1' => [
-        'type' => 'none',
-        'user' => [
-          'about1None',
-        ],
-        'emotion' => 'thumbs-up',
+  'day' => null,
+  'category' => 'other',
+
+  'wanda' => [
+    'about1' => [
+      'type' => 'none',
+      'user' => [
+        'about1None',
       ],
-      'about2' => [
-        'type' => 'none',
-        'user' => [
-          'about2None',
-        ],
-      ],
-      'about3' => [
-        'type' => 'none',
-        'user' => [
-          'about3None',
-        ],
-      ],
-      'about4' => [
-        'type' => 'none',
-        'user' => [
-          'about4None',
-        ],
-      ],
-      'about5' => [
-        'type' => 'none',
-        'user' => [
-          'about5None',
-        ],
-      ],
-      'about6' => [
-        'type' => 'none',
-        'user' => [
-          'about6None',
-        ],
-      ],
-      'about6a' => [
-        'type' => 'choice',
-        'user' => [
-          'aboutWho',
-        ],
-      ],
-      'about7' => [
-        'type' => 'none',
-        'user' => [
-          'about7None',
-        ],
-      ],
-      'about8' => [
-        'type' => 'choice',
-        'user' => [
-          'aboutThanks',
-        ],
-      ],
-      'about9' => [
-        'type' => 'none',
-        'user' => [
-          'about9None',
-        ],
-        'emotion' => 'heart',
-      ],
-      'about10' => [
-        'type' => 'none',
-        'user' => [
-          'about10None',
-        ],
-      ],
-      'about11' => [
-        'type' => 'none',
-        'user' => [
-          'about11None',
-        ],
-      ],
-      'about12' => [
-        'type' => 'none',
-        'user' => [
-          'about12None',
-        ],
-      ],
-      'about13' => [
-        'type' => 'none',
-        'user' => [
-          'about13None',
-        ],
-      ],
-      'about14' => [
-        'type' => 'none',
-        'user' => [
-          'about14None',
-        ],
-      ],
-      'about15' => [
-        'type' => 'none',
-        'user' => [
-          'about15None',
-        ],
-      ],
-      'about16' => [
-        'type' => 'none',
-        'user' => [
-          'about16None',
-        ],
-      ],
-      'about17' => [
-        'type' => 'none',
-        'user' => [
-          'about17None',
-        ],
-      ],
-      'about18' => [
-        'type' => 'none',
-        'user' => [
-          'about18None',
-        ],
-      ],
-      'about19' => [
-        'type' => 'end',
-        'user' => [
-          'about19None',
-        ],
+      'emotion' => 'thumbs-up',
+    ],
+    'about2' => [
+      'type' => 'none',
+      'user' => [
+        'about2None',
       ],
     ],
-  
-    'user' => [
-      'begin' => [
-        'wanda' => 'about1',
-      ],
-      'about1None' => [
-        'wanda' => 'about2',
-      ],
-      'about2None' => [
-        'wanda' => 'about3',
-      ],
-      'about3None' => [
-        'wanda' => 'about4',
-      ],
-      'about4None' => [
-        'wanda' => 'about5',
-      ],
-      'about5None' => [
-        'wanda' => 'about6',
-      ],
-      'about6None' => [
-        'wanda' => 'about6a',
-      ],
-      'aboutWho' => [
-        'wanda' => 'about7',
-      ],
-      'about7None' => [
-        'wanda' => 'about8',
-      ],
-      'aboutThanks' => [
-        'wanda' => 'about9',
-      ],
-      'about9None' => [
-        'wanda' => 'about10',
-      ],
-      'about10None' => [
-        'wanda' => 'about11',
-      ],
-      'about11None' => [
-        'wanda' => 'about12',
-      ],
-      'about12None' => [
-        'wanda' => 'about13',
-      ],
-      'about13None' => [
-        'wanda' => 'about14',
-      ],
-      'about14None' => [
-        'wanda' => 'about15',
-      ],
-      'about15None' => [
-        'wanda' => 'about16',
-      ],
-      'about16None' => [
-        'wanda' => 'about17',
-      ],
-      'about17None' => [
-        'wanda' => 'about18',
-      ],
-      'about18None' => [
-        'wanda' => 'about19',
-      ],
-      'about19None' => [
-        'wanda' => '',
+    'about3' => [
+      'type' => 'none',
+      'user' => [
+        'about3None',
       ],
     ],
+    'about4' => [
+      'type' => 'none',
+      'user' => [
+        'about4None',
+      ],
+    ],
+    'about5' => [
+      'type' => 'none',
+      'user' => [
+        'about5None',
+      ],
+    ],
+    'about6' => [
+      'type' => 'none',
+      'user' => [
+        'about6None',
+      ],
+    ],
+    'about6a' => [
+      'type' => 'choice',
+      'user' => [
+        'aboutWho',
+      ],
+    ],
+    'about7' => [
+      'type' => 'none',
+      'user' => [
+        'about7None',
+      ],
+    ],
+    'about8' => [
+      'type' => 'choice',
+      'user' => [
+        'aboutThanks',
+      ],
+    ],
+    'about9' => [
+      'type' => 'none',
+      'user' => [
+        'about9None',
+      ],
+      'emotion' => 'heart',
+    ],
+    'about10' => [
+      'type' => 'none',
+      'user' => [
+        'about10None',
+      ],
+    ],
+    'about11' => [
+      'type' => 'none',
+      'user' => [
+        'about11None',
+      ],
+    ],
+    'about12' => [
+      'type' => 'none',
+      'user' => [
+        'about12None',
+      ],
+    ],
+    'about13' => [
+      'type' => 'none',
+      'user' => [
+        'about13None',
+      ],
+    ],
+    'about14' => [
+      'type' => 'none',
+      'user' => [
+        'about14None',
+      ],
+    ],
+    'about15' => [
+      'type' => 'none',
+      'user' => [
+        'about15None',
+      ],
+    ],
+    'about16' => [
+      'type' => 'none',
+      'user' => [
+        'about16None',
+      ],
+    ],
+    'about17' => [
+      'type' => 'none',
+      'user' => [
+        'about17None',
+      ],
+    ],
+    'about18' => [
+      'type' => 'none',
+      'user' => [
+        'about18None',
+      ],
+    ],
+    'about19' => [
+      'type' => 'end',
+      'user' => [
+        'about19None',
+      ],
+    ],
+  ],
+
+  'user' => [
+    'begin' => [
+      'wanda' => 'about1',
+    ],
+    'about1None' => [
+      'wanda' => 'about2',
+    ],
+    'about2None' => [
+      'wanda' => 'about3',
+    ],
+    'about3None' => [
+      'wanda' => 'about4',
+    ],
+    'about4None' => [
+      'wanda' => 'about5',
+    ],
+    'about5None' => [
+      'wanda' => 'about6',
+    ],
+    'about6None' => [
+      'wanda' => 'about6a',
+    ],
+    'aboutWho' => [
+      'wanda' => 'about7',
+    ],
+    'about7None' => [
+      'wanda' => 'about8',
+    ],
+    'aboutThanks' => [
+      'wanda' => 'about9',
+    ],
+    'about9None' => [
+      'wanda' => 'about10',
+    ],
+    'about10None' => [
+      'wanda' => 'about11',
+    ],
+    'about11None' => [
+      'wanda' => 'about12',
+    ],
+    'about12None' => [
+      'wanda' => 'about13',
+    ],
+    'about13None' => [
+      'wanda' => 'about14',
+    ],
+    'about14None' => [
+      'wanda' => 'about15',
+    ],
+    'about15None' => [
+      'wanda' => 'about16',
+    ],
+    'about16None' => [
+      'wanda' => 'about17',
+    ],
+    'about17None' => [
+      'wanda' => 'about18',
+    ],
+    'about18None' => [
+      'wanda' => 'about19',
+    ],
+    'about19None' => [
+      'wanda' => '',
+    ],
+  ],
 
     
 ];
