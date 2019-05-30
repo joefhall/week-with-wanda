957bd19ad9a70cf4208b925c5777c5e31c120655
Joe Hall <joefhall@gmail.com>
2019-05-30 10:18:49 +0000

    Fix hashtag in sharing

--- resources/js/components/ChatInputShare.js
+++ resources/js/components/ChatInputShare.js
@@ -10,7 +10,7 @@ export default class ChatInputShare extends React.Component {
               <img className="chat__input__choices__choice__share__icon" alt="Facebook logo" src="/img/share/facebook.svg" />
               Share
             </a>
-            <a className="chat__input__choices__choice chat__input__choices__choice__share--twitter" href={`https://twitter.com/intent/tweet?url=https%3A%2F%2Fweekwithwanda.com%2F%3Fshare%3D${this.props.scenario}&text=I%27m%20playing%20this%20hilarious%20game...%20an%20AI%20bot%20seems%20to%20be%20taking%20over%20my%20life%21&hashtags=ai%20weekwithwanda`} key="twitter" target="_blank">
+            <a className="chat__input__choices__choice chat__input__choices__choice__share--twitter" href={`https://twitter.com/intent/tweet?url=https%3A%2F%2Fweekwithwanda.com%2F%3Fshare%3D${this.props.scenario}&text=I%27m%20playing%20this%20hilarious%20game...%20an%20AI%20bot%20seems%20to%20be%20taking%20over%20my%20life%21&hashtags=ai%2Cweekwithwanda`} key="twitter" target="_blank">
               <img className="chat__input__choices__choice__share__icon" alt="Twitter logo" src="/img/share/twitter.svg" />
               Tweet
             </a>
